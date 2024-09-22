c258e67bd5adb21909db43814c09e171cf3dbc06
Anirban Chowdhury <ani_anirban@yahoo.com>
2024-09-22 22:29:27 +0800

    OMS test - part 1

--- src/test/java/org/concurrencyTest/ConcurrencySuiteSubmitToOMS.java
+++ src/test/java/org/concurrencyTest/ConcurrencySuiteSubmitToOMS.java
@@ -1,8 +1,10 @@
 package org.concurrencyTest;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.concurrencyTest.dto.AllocationMessage;
+import org.concurrencyTest.dto.ExpectedPosition;
 import org.concurrencyTest.dto.PMResponseMessage;
 import org.concurrencyTest.entity.ExpectedTransaction;
 import org.concurrencyTest.repository.ExpectedTransactionsRepository;
@@ -11,10 +13,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
 import java.util.List;
@@ -52,6 +56,14 @@ public class ConcurrencySuiteSubmitToOMS {
     private static final Logger logger = LoggerFactory.getLogger(ConcurrencySuiteSubmitToOMS.class);
     private LinkedBlockingQueue<String> pmResponseQueue = new LinkedBlockingQueue<>();
 
+    private final RestTemplate restTemplate;
+
+    @Autowired
+    public ConcurrencySuiteSubmitToOMS(RestTemplateBuilder restTemplateBuilder) {
+        this.restTemplate = restTemplateBuilder.build();
+    }
+
+
     @KafkaListener(topics = PM_TOPIC, groupId = "oms-group")
     public void listenToPMTopic(String message) {
         logger.info("received a message on the PM topic {}", message);
@@ -66,19 +78,38 @@ public class ConcurrencySuiteSubmitToOMS {
                         expectedTransaction.getProduct().getProductName(),"USD",expectedTransaction.getDirection(),expectedTransaction.getQuantity(),0);
                 String jsonString = objectMapper.writeValueAsString(message);
                 kafkaTemplate.send(new ProducerRecord<>(OMS_TOPIC, jsonString));
-                //assert on  response
+
                 //Assert on PM response
                 String pmResponseMessage = pmResponseQueue.poll(5, TimeUnit.SECONDS);  // Wait for the PM response
                 assertThat(pmResponseMessage).isNotNull();
-
                 PMResponseMessage pmResponse = objectMapper.readValue(pmResponseMessage, PMResponseMessage.class);
                 assertThat(pmResponse.getSourceOrderId()).isEqualTo(expectedTransaction.getOrderId());
-
                 assertThat(pmResponse.getStatus()).isEqualTo(PENDING_EXECUTION.name());
 
-                //wait for a while till the positionAggregation completes
-                //compute positions aggregated from expected_tx and compare against positions returned
+                logger.info("received PM response for {}", pmResponse);
                 //if OK, update expected_tx pending to false
+                expectedTransaction.setPending(false);
+                expectedTransactionsRepository.save(expectedTransaction);
+
+                //Send tx to PositionAggregator ?
+
+                //wait for a while till the positionAggregation completes
+                //Now scan across all expected_transactions to compute positions
+                String positionsApiUrl = "http://localhost:8085/api/positions";
+
+                // Deserialize the JSON array into a List of ExpectedPosition objects
+                String jsonResponse = restTemplate.getForObject(positionsApiUrl, String.class);
+
+                List<ExpectedPosition> expectedPositions = objectMapper.readValue(jsonResponse, new TypeReference<>() {
+                });
+                expectedPositions.stream()
+                        .filter(expectedPosition ->
+                                expectedPosition.getAccount().getAccountName().equals(expectedTransaction.getAccount().getAccountName())
+                                && expectedPosition.getProduct().getProductName().equals(expectedTransaction.getProduct().getProductName()));
+
+                logger.info("Filtered expected position = {}",expectedPositions);
+                //compute positions aggregated from expected_tx and compare against positions returned
+
 
 
             }
